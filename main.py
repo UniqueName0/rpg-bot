5c67552af29be4e0b8efc84be29c99c3be1e6a52
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2021-01-08 15:34:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,9 @@ async def stats(ctx):
 async def arena(ctx,arg):
     user = ctx.author
     await create_account(user)
-    hp = rpgdata.execute("select health from rpgdb where userID=?", (user.id,)).fetchone()
+    hp1 = rpgdata.execute("select health from rpgdb where userID=?", (user.id,)).fetchone()
+    hp = "".join(c for c in hp1 if  c.isdecimal())
+    hp = int(hp)
     dmg = rpgdata.execute("select damage from rpgdb where userID=?", (user.id,)).fetchone()
     defense = rpgdata.execute("select armor from rpgdb where userID=?", (user.id,))
     dodge = rpgdata.execute("select evasion from rpgdb where userID=?",  (user.id,))
