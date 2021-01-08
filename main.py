967459752a0ef5fbf28028684277afdb49c0e644
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2021-01-07 20:18:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ async def stats(ctx):
     user = ctx.author
     await create_account(user)
     em = discord.Embed(title = f"{ctx.author.name}'s stats", color = discord.Color.red())
-    gold = rpgdata.execute("SELECT gold FROM rpgdb WHERE userID = ?", (user.id,)).fetchone())
-    em.add_field(name = "gold", value = gold.translate({ord(c): None for c in '!@#$,'})
+    em.add_field(name = "gold", value = rpgdata.execute("SELECT gold FROM rpgdb WHERE userID = ?", (user.id,)).fetchone()).translate({ord(c): None for c in '!@#$,'})
     em.add_field(name = "level", value = rpgdata.execute("SELECT level FROM rpgdb WHERE userID = ?", (user.id,)).fetchone())
     await ctx.send(embed = em)
 
